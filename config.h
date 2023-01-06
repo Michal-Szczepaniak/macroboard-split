f32b67ed5aa1fc15fb2bb2d6b54a6edcd7e7b437
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2023-01-06 14:59:39 +0100

    Fix keyboard layout

--- config.h
+++ config.h
@@ -21,8 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SERIAL_USART_TX_PIN B6
 
-#define EE_HANDS
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
